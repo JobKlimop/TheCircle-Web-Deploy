c99d7b6ee7f2a41db6330e5cb6ba2ee3e52985b5
Giede van Gils <giedevangils@gmail.com>
2018-06-21 20:21:03 +0200

    Attempt 49

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  accountApiUrl: 'https://the-circle-account.herokuapp.com/api/account'
+  accountApiUrl: 'https://the-circle-account.herokuapp.com/api/account',
+  videoDataApiUrl: 'http:///145.49.27.5:8000/api',
+  videoStreamApiUrl: 'http:///145.49.27.5:8000/live'
 };
