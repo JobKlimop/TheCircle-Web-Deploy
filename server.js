dbeec43ae78001d00590c4b6831850df8624131c
Giede van Gils <giedevangils@gmail.com>
2018-06-21 20:34:57 +0200

    Attempt 52

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, ', dist/index.html'));
