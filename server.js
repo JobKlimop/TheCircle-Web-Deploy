637eb7c0708dc3b6055810a85f98f545eb071074
Giede van Gils <giedevangils@gmail.com>
2018-06-21 20:39:42 +0200

    Attempt 53

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, ', dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const port = process.env.PORT || '4200';
