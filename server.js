67348cbee42da75be1b4da841e31567691280d1f
Giede van Gils <giedevangils@gmail.com>
2018-06-21 18:09:43 +0200

    Attempt 28

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join('./dist/seechange/index.html'));
 });
 
+const port = process.env.PORT || '4200';
+app.set('port', port);
+
 app.listen(port, () => console.log(`Angular server running on port ${port}`));
